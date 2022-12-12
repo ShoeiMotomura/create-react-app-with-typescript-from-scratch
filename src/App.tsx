7ce82d2f370a894837bc49bd346749e08d320e41
ShoeiMotomura <s-motomura@nri.co.jp>
2022-12-12 22:46:06 +0900

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button';
 
 const App = () => {
   return (
     <div className='App'>
       <h1> Hello, World!</h1>
+      <Button>Button</Button>
     </div>
   );
 };
